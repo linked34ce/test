5f0a69b6ff6eb2a1f5fb6f334e460642db1d55e1
EC2 Default User <ec2-user@ip-172-31-30-227.ap-northeast-1.compute.internal>
2021-08-26 06:27:26 +0000

    ruby first challenge

---
+++ udemy.rb
@@ -0,0 +1,21 @@
+# puts "Hello, World!!"
+
+# a = 1
+# b = 2
+# c = a + b
+
+# puts a
+# puts b
+# puts c
+# puts a + b
+
+# a = "string1"
+# b = "string2"
+
+# puts a
+# puts b
+
+a = 123
+b = "The number is #{a}"
+
+puts b
